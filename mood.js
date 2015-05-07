608a48a8a1c57efe70d14695865b58c575f849bd
MaciekTalaska <maciek.talaska@gmail.com>
2015-05-07 23:12:45 +0000

    counting clicks on each mood type

--- mood.js
+++ mood.js
@@ -1,17 +1,33 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+  Session.setDefault('red', 0);
+  Session.setDefault('yellow', 0);
+  Session.setDefault('green', 0);
 
   Template.status.helpers({
-    counter: function () {
-      return Session.get('counter');
+    red: function() {
+      return Session.get('red');
+    },
+    yellow: function() {
+      return Session.get('yellow');
+    },
+    green: function() {
+      return Session.get('green');
     }
   });
 
   Template.moodsInCircles.events( {
-    'click svg': function () {
-      Session.set('counter', Session.get('counter') + 1);
-    }
+    'click circle': function(e) {
+      var moodType = e.target.id;
+      if (moodType === "moodGreen") {
+        Session.set('green', Session.get('green') +1);
+      }
+      if (moodType === "moodYellow") {
+        Session.set('yellow', Session.get('yellow') +1);
+      }
+      if (moodType === "moodRed") {
+        Session.set('red', Session.get('red') +1);
+      }
+  }
   });
 }
 
