0fea34bf8c0b3027cd41e4b124ac96c94345ccf4
MaciekTalaska <maciek.talaska@gmail.com>
2015-05-09 19:55:05 +0000

    fancier toasters

--- mood.js
+++ mood.js
@@ -20,9 +20,15 @@ if (Meteor.isClient) {
   });
 
   Helpers = {
+    getRandomEffect : function() {
+      var notifications = ['scale', 'slide', 'genie', 'jelly', 'flip', 'bouncyflip', 'stackslide'];
+      var number = Math.floor(Math.random()*7+1);
+      return notifications[number];
+    },
     incrementSessionKey: function(key) {
       Session.set(key, Session.get(key) +1);
-      sAlert.success('Thanks for voting!', {timeout: 2000});
+      var effectName = this.getRandomEffect();
+      sAlert.success('Thanks for voting!', {timeout: 2000, position: 'bottom-right', effect: effectName});
     }
   }
   
